a9de4315ec7224a4bfc869719eccb9f7ec8ca040
Garland Lai and Mike Lofgren <mike@>
2019-06-19 09:36:02 -0700

    add intitial html and directories for our pig dice game

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//business logic for pig dice game
+function PigDicePlayer() {
+  this.player = [];
+}
+
+PigDicePlayer.prototype.addPlayer = function () {
+  this.player.push(player);
+};
+
+//business logic for pig dice game
+function player() {
+  this.userName = userName,
+  this.totalScore = totalScore,
+  this.dice = dice,
+  this.currentTurnScore = currentTurnScore;
+}
