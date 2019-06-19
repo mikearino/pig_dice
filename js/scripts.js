44fafd9484431e774264e18b15e047277cd41d6a
Garland Lai and Mike Lofgren <mike@>
2019-06-19 11:57:13 -0700

    add functionality for two players. Hiding and showing divs with jQuety and Css.

--- js/scripts.js
+++ js/scripts.js
@@ -8,27 +8,52 @@ PigDicePlayer.prototype.addPlayer = function () {
 };
 
 //business logic for pig dice game
-function Player(userName, totalScore, diceFace, currentTurnScore) {
+function Player(userName, totalScore, diceFace, currentScore) {
   this.userName = userName,
   this.totalScore = totalScore,
   this.diceFace = diceFace,
-  this.currentTurnScore = currentTurnScore;
+  this.currentScore = currentScore;
 }
 
 //user logic
 var pigDicePlayerForList = new PigDicePlayer();
 
 $(document).ready(function () {
-  $('form#name-entry').submit(function (event) {
+  $('form#name-entry1').submit(function (event) {
     event.preventDefault();
-    var playerOneNameInput = $('#new-user').val();
+    var playerOneNameInput = $('#new-user1').val();
     console.log(playerOneNameInput);
     var totalScore = [];
     var diceFace = [];
     var currentScore = [];
 
+    //this creating a new player with the new Player constructor
     var newPlayer = new Player(playerOneNameInput, totalScore, diceFace, currentScore);
-    console.log(newPlayer);
+
+    //this calls the prototype to push new player into  player array
     pigDicePlayerForList.addPlayer(newPlayer);
+    //This hides the form after name submission
+    $('#name-entry1').hide();
+    //This shows player interface
+    $('#show-score1').show();
+  });
+
+  $('form#name-entry2').submit(function (event) {
+    event.preventDefault();
+    var playerTwoNameInput = $('#new-user2').val();
+    console.log(playerTwoNameInput);
+    var totalScore = [];
+    var diceFace = [];
+    var currentScore = [];
+
+    //this creating a new player with the new Player constructor
+    var newPlayer = new Player(playerTwoNameInput, totalScore, diceFace, currentScore);
+
+    //this calls the prototype to push new player into  player array
+    pigDicePlayerForList.addPlayer(newPlayer);
+    //This hides the form after name submission
+    $('#name-entry2').hide();
+    //This shows player interface
+    $('#show-score2').show();
   });
 });
