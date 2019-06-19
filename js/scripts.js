3652294ad3ea887181ef1a1462c66b92bb9ea047
Garland Lai and Mike Lofgren <garland@>
2019-06-19 14:50:12 -0700

    add functional dice roll and output

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,16 @@ PigDicePlayer.prototype.addPlayer = function (Player) {
   this.player.push(Player);
 };
 
+function rollingDice() {
+  return Math.floor(Math.random() * ((6 - 1) + 1) + 1);
+}
+
 //business logic for pig dice game
 function Player(userName, totalScore, diceFace, currentScore) {
   this.userName = userName;
   this.totalScore = totalScore;
   this.diceFace = diceFace;
-  this.currentScore = currentScore;
+  this.currentScore = 0;
 }
 
 //user logic
@@ -36,12 +40,15 @@ $(document).ready(function () {
     $('#name-entry1').hide();
     //This shows player interface
     $('#show-score1').show();
+    $('#name-entry2').show();
+
   });
 
   $('form#name-entry2').submit(function (event) {
     event.preventDefault();
     var playerTwoNameInput = $('#new-user2').val();
     console.log(playerTwoNameInput);
+
     var totalScore = [];
     var diceFace = [];
     var currentScore = [];
@@ -55,5 +62,22 @@ $(document).ready(function () {
     $('#name-entry2').hide();
     //This shows player interface
     $('#show-score2').show();
+  });
+
+
+  $('#roll1').click(function (event) {
+    event.preventDefault();
+    var currentRoll = rollingDice();
+    $('#current-dice1').text(currentRoll);
+    pigDicePlayerForList.player[0].currentScore += currentRoll
+    $('#current-turn-score1').text(pigDicePlayerForList.player[0].currentScore);
+  });
+
+  $('#roll2').click(function (event) {
+    event.preventDefault();
+    var currentRoll = rollingDice();
+    $('#current-dice2').text(currentRoll);
+    pigDicePlayerForList.player[1].currentScore += currentRoll
+    $('#current-turn-score2').text(pigDicePlayerForList.player[1].currentScore);
   });
 });
