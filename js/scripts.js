71af2fd0f0e50e9182a302a16b48354bd25a7984
Garland Lai and mike lofgren <mike@>
2019-06-20 12:01:40 -0700

    add dom manipulation fix hold.

--- js/scripts.js
+++ js/scripts.js
@@ -18,10 +18,11 @@ function Player(userName, totalScore, diceFace, currentScore) {
   this.diceFace = 0;
   this.currentScore = 0;
 }
-//Working on hold prototype
-// Player.prototype.holdFunc = function() {
-//   this.totalScore += currentScore
-// }
+
+Player.prototype.holdFunc = function() {
+  this.totalScore += this.currentScore
+  this.currentScore = 0
+}
 
 Player.prototype.rollDice = function() {
   if (this.diceFace === 1) {
@@ -38,6 +39,7 @@ $(document).ready(function () {
   $('form#name-entry1').submit(function (event) {
     event.preventDefault();
     var playerOneNameInput = $('#new-user1').val();
+      $('#name-turn-indicator1').text(playerOneNameInput)
     console.log(playerOneNameInput);
     var totalScore = [];
     var diceFace = [];
@@ -59,7 +61,7 @@ $(document).ready(function () {
   $('form#name-entry2').submit(function (event) {
     event.preventDefault();
     var playerTwoNameInput = $('#new-user2').val();
-    console.log(playerTwoNameInput);
+    $('#name-turn-indicator2').text(playerTwoNameInput);
     var totalScore = [];
     var diceFace = [];
     var currentScore = [];
@@ -85,7 +87,7 @@ $(document).ready(function () {
     var currentRoll = rollingDice();
     $('#current-dice1').text(currentRoll);
     pigDicePlayerForList.player[0].diceFace = currentRoll
-    pigDicePlayerForList.player[0].currentScore += currentRoll
+    // pigDicePlayerForList.player[0].currentScore += currentRoll
     pigDicePlayerForList.player[0].rollDice();
     $('#current-turn-score1').text(pigDicePlayerForList.player[0].currentScore);
   });
@@ -96,7 +98,7 @@ $(document).ready(function () {
     var currentRoll = rollingDice();
     $('#current-dice2').text(currentRoll);
     pigDicePlayerForList.player[1].diceFace = currentRoll
-    pigDicePlayerForList.player[1].currentScore += currentRoll
+    // pigDicePlayerForList.player[1].currentScore += currentRoll
     pigDicePlayerForList.player[1].rollDice();
     $('#current-turn-score2').text(pigDicePlayerForList.player[1].currentScore);
   });
@@ -104,21 +106,21 @@ $(document).ready(function () {
   //This click function calculates the total score and creates output to the DOM
   //and updates objects
   $('#hold1').click(function (event) {
-    event.preventDefault();
-    var currentScore = pigDicePlayerForList.player[0].currentScore
-     pigDicePlayerForList.player[0].totalScore += currentScore;
-     $('#total-score1').text(currentScore);
+      event.preventDefault();
+      pigDicePlayerForList.player[0].holdFunc();
+     $('#total-score1').text(pigDicePlayerForList.player[0].totalScore);
      $('#button1').hide();
      $('#button2').show();
+     $('#current-turn-score1').text(pigDicePlayerForList.player[0].currentScore);
   });
 
   $('#hold2').click(function (event) {
     event.preventDefault();
-    var currentScore = pigDicePlayerForList.player[1].currentScore
-     pigDicePlayerForList.player[1].totalScore += currentScore;
-   $('#total-score2').text(currentScore);
+    pigDicePlayerForList.player[1].holdFunc();
+   $('#total-score2').text(pigDicePlayerForList.player[1].totalScore);
    $('#button2').hide();
    $('#button1').show();
+  $('#current-turn-score2').text(pigDicePlayerForList.player[1].currentScore);
   });
 
 });
